e48bb93853c2424378667abcf9056b3408224acb
jaffbala75 <126783242+jaffbala75@users.noreply.github.com>
2024-07-16 09:33:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,11 +15,11 @@ provider "aws" {
 
 resource "aws_instance" "vm1" {
   ami                         = "ami-0ebf330ebbfac441d"
-  subnet_id                   = "subnet-027e48d7954f1d051"
+  subnet_id                   = "subnet-0e1bbe34aea7d7195"
   instance_type               = "t2.micro"
   associate_public_ip_address = true
   key_name                    = "tkey"
-  security_groups             = ["sg-06c31a4f30402c865"]
+  security_groups             = ["sg-0857712effe8a49bd"]
   #user_data                   = file("./ntlm_enable.bat")
   get_password_data           = true
 }
